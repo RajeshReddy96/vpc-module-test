bba84611fec7c708f3f8b6323d288c2112c79299
Rajeshreddyk <rajeshreddy.k.315@gmail.com>
2024-09-28 14:32:05 +0530

    terraform

--- variables.tf
+++ variables.tf
@@ -18,9 +18,13 @@ variable "public_subnet_cidrs" {
 }
 
 variable "private_subnet_cidrs" {
-    default = ["10.0.11.0/24", "10.0.21.0/24"]
+    default = ["10.0.11.0/24", "10.0.12.0/24"]
 }
 
 variable "database_subnet_cidrs" {
     default = ["10.0.21.0/24", "10.0.22.0/24"]
+}
+
+variable "is_peering_required" {
+    default = true
 }
